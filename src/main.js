7673f8498c608d42d125576fd7ae0f628a2867f2
Philippe Enzler <phil@enzler.dev>
2024-05-06 13:14:58 -0700

    Use Geolocation API

--- src/main.js
+++ src/main.js
@@ -24,74 +24,88 @@ const parts = {
   }
 } */
 
-let location = {
-  lat: 34.156412379355466,
-  lng: -118.42623338789605
+let location
+
+async function getLocation() {
+  await navigator.geolocation.getCurrentPosition(setLocation, (error) => console.log(error))
 }
 
-/* function setLocation(pos) {
+function setLocation(pos) {
   location = [pos.latitude, pos.longitude]
   console.log(location)
+  updateSunTimes()
 }
-// await navigator.geolocation.getCurrentPosition(setLocation, (error) => console.log(error))
- */
 
 function calculateDayPart(dateTime) {
   return dateTime.hour / 24 + (dateTime.minute / (24 * 60)) + (dateTime.second / (24 * 60 * 60))
 }
 
+function getCronTimeForEvery12HoursFromNow() {
+  const now = DateTime.now()
+  const hours = now.hour < 12 ? [now.hour, now.hour + 12] : [now.hour - 12, now.hour]
+  return `${now.minute} ${hours.join(",")} * * *`
+}
+
+function updateSunTimes() {
+  if (location === undefined) {
+    return
+  }
+
+  console.info(`Updating sun times for ${location.lat}, ${location.lng}.`)
+  const apiUrl = `https://api.sunrisesunset.io/json?lat=${location.lat}&lng=${location.lng}`
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.results)
+      return {
+        firstLight: DateTime.fromFormat(`${data.results.date} ${data.results.first_light}`, 'yyyy-MM-dd tt', { zone: data.results.timezone }),
+        dawn: DateTime.fromFormat(`${data.results.date} ${data.results.dawn}`, 'yyyy-MM-dd tt', { zone: data.results.timezone }),
+        sunrise: DateTime.fromFormat(`${data.results.date} ${data.results.sunrise}`, 'yyyy-MM-dd tt', { zone: data.results.timezone }),
+        sunset: DateTime.fromFormat(`${data.results.date} ${data.results.sunset}`, 'yyyy-MM-dd tt', { zone: data.results.timezone }),
+        dusk: DateTime.fromFormat(`${data.results.date} ${data.results.dusk}`, 'yyyy-MM-dd tt', { zone: data.results.timezone }),
+        lastLight: DateTime.fromFormat(`${data.results.date} ${data.results.last_light}`, 'yyyy-MM-dd tt', { zone: data.results.timezone })
+      }
+    })
+    .then(sunTimes => updateSunGraph(sunTimes))
+    .catch(error => console.error('Error fetching sunrise and sunset data:', error))
+}
+
+function updateSunGraph(sunTimes) {
+  console.log(sunTimes)
+
+  const indicators = {
+    firstLight: document.getElementById('first-light-indicator'),
+    dawn: document.getElementById('dawn-indicator'),
+    daytime: document.getElementById('daytime-indicator'),
+    dusk: document.getElementById('dusk-indicator'),
+    lastLight: document.getElementById('last-light-indicator')
+  }
+
+  const dayParts = {
+    firstLight: calculateDayPart(sunTimes.firstLight),
+    dawn: calculateDayPart(sunTimes.dawn),
+    sunrise: calculateDayPart(sunTimes.sunrise),
+    sunset: calculateDayPart(sunTimes.sunset),
+    dusk: calculateDayPart(sunTimes.dusk),
+    lastLight: calculateDayPart(sunTimes.lastLight)
+  }
+
+  indicators.firstLight.style.left = `${dayParts.firstLight * 100}%`
+  indicators.firstLight.style.width = `${(dayParts.dawn - dayParts.firstLight) * 100}%`
+  indicators.dawn.style.left = `${dayParts.dawn * 100}%`
+  indicators.dawn.style.width = `${(dayParts.sunrise - dayParts.dawn) * 100}%`
+  indicators.daytime.style.left = `${dayParts.sunrise * 100}%`
+  indicators.daytime.style.width = `${(dayParts.sunset - dayParts.sunrise) * 100}%`
+  indicators.dusk.style.right = `${(1 - dayParts.dusk) * 100}%`
+  indicators.dusk.style.width = `${(dayParts.dusk - dayParts.sunset) * 100}%`
+  indicators.lastLight.style.right = `${(1 - dayParts.lastLight) * 100}%`
+  indicators.lastLight.style.width = `${(dayParts.lastLight - dayParts.dusk) * 100}%`
+}
+
 const cronJobs = [
   CronJob.from({
-    cronTime: '42 * * * *',
-    onTick: async (onComplete) => {
-      console.info(`Updating sun times for ${location.lat}, ${location.lng}.`)
-      const apiUrl = `https://api.sunrisesunset.io/json?lat=${location.lat}&lng=${location.lng}`
-      await fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-          console.log(data.results)
-          return {
-            firstLight: DateTime.fromFormat(`${data.results.date} ${data.results.first_light}`, 'yyyy-MM-dd tt', { zone: data.results.timezone }),
-            dawn: DateTime.fromFormat(`${data.results.date} ${data.results.dawn}`, 'yyyy-MM-dd tt', { zone: data.results.timezone }),
-            sunrise: DateTime.fromFormat(`${data.results.date} ${data.results.sunrise}`, 'yyyy-MM-dd tt', { zone: data.results.timezone }),
-            sunset: DateTime.fromFormat(`${data.results.date} ${data.results.sunset}`, 'yyyy-MM-dd tt', { zone: data.results.timezone }),
-            dusk: DateTime.fromFormat(`${data.results.date} ${data.results.dusk}`, 'yyyy-MM-dd tt', { zone: data.results.timezone }),
-            lastLight: DateTime.fromFormat(`${data.results.date} ${data.results.last_light}`, 'yyyy-MM-dd tt', { zone: data.results.timezone })
-          }
-        })
-        .then(sunTimes => onComplete(sunTimes))
-        .catch(error => console.error('Error fetching sunrise and sunset data:', error))
-    },
-    onComplete: (sunTimes) => {
-      console.log(sunTimes)
-      const indicators = {
-        firstLight: document.getElementById('first-light-indicator'),
-        dawn: document.getElementById('dawn-indicator'),
-        daytime: document.getElementById('daytime-indicator'),
-        dusk: document.getElementById('dusk-indicator'),
-        lastLight: document.getElementById('last-light-indicator')
-      }
-
-      const dayParts = {
-        firstLight: calculateDayPart(sunTimes.firstLight),
-        dawn: calculateDayPart(sunTimes.dawn),
-        sunrise: calculateDayPart(sunTimes.sunrise),
-        sunset: calculateDayPart(sunTimes.sunset),
-        dusk: calculateDayPart(sunTimes.dusk),
-        lastLight: calculateDayPart(sunTimes.lastLight)
-      }
-
-      indicators.firstLight.style.left = `${dayParts.firstLight * 100}%`
-      indicators.firstLight.style.width = `${(dayParts.dawn - dayParts.firstLight) * 100}%`
-      indicators.dawn.style.left = `${dayParts.dawn * 100}%`
-      indicators.dawn.style.width = `${(dayParts.sunrise - dayParts.dawn) * 100}%`
-      indicators.daytime.style.left = `${dayParts.sunrise * 100}%`
-      indicators.daytime.style.width = `${(dayParts.sunset - dayParts.sunrise) * 100}%`
-      indicators.dusk.style.right = `${(1 - dayParts.dusk) * 100}%`
-      indicators.dusk.style.width = `${(dayParts.dusk - dayParts.sunset) * 100}%`
-      indicators.lastLight.style.right = `${(1 - dayParts.lastLight) * 100}%`
-      indicators.lastLight.style.width = `${(dayParts.lastLight - dayParts.dusk) * 100}%`
-    },
+    cronTime: getCronTimeForEvery12HoursFromNow(),
+    onTick: getLocation,
     start: true,
     runOnInit: true
   })
@@ -131,5 +145,9 @@ function tick() {
 
   setTimeout(tick, 1000 - now.millisecond)
 }
+
+document.body.addEventListener('click', () => {
+  getCurrentLocation()
+})
 
 tick()
